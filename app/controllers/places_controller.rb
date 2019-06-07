bd6866fe166b574a71363d767448e9166d91430a
cw2981 <cw2981>
2019-06-07 18:13:42 +0000

    error on heroku server testing possible fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,7 +21,12 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
   end
-    
+  
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
 
   private
