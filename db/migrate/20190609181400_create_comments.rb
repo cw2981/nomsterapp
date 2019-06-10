11bc10959a7e2940daa4b81b944f1ab9dd95d704
cw2981 <cw2981>
2019-06-10 20:57:09 +0000

    saving data incase error fix does not work

--- db/migrate/20190609181400_create_comments.rb
+++ db/migrate/20190609181400_create_comments.rb
@@ -6,7 +6,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
       t.integer :user_id
       t.integer :place_id
       t.timestamps
-    end
+end
     
     add_index :comments, [:user_id, :place_id]
     add_index :comments, :place_id
