0c1bf04d30d48578b0fe10e4c2cf85ef7d362e40
cw2981 <cw2981>
2019-06-27 20:51:17 +0000

    setting simple form for photo upload.

--- db/migrate/20190620000530_add_picture_to_photos.rb
+++ db/migrate/20190620000530_add_picture_to_photos.rb
@@ -3,6 +3,5 @@ class AddPictureToPhotos < ActiveRecord::Migration[5.2]
     add_column :photos, :picture, :string
     add_column :photos, :caption, :string
     add_column :photos, :place_id, :integer
-    add_column :photos, :user_id, :integer
   end
 end
