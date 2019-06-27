f9fa13d9e085aab8a0189cd2f2530014e0a082a6
cw2981 <cw2981>
2019-06-27 20:23:10 +0000

    migrating user id to photos column.

--- db/migrate/20190620000530_add_picture_to_photos.rb
+++ db/migrate/20190620000530_add_picture_to_photos.rb
@@ -3,5 +3,6 @@ class AddPictureToPhotos < ActiveRecord::Migration[5.2]
     add_column :photos, :picture, :string
     add_column :photos, :caption, :string
     add_column :photos, :place_id, :integer
+    add_column :photos, :user_id, :integer
   end
 end
