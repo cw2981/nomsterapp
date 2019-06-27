73fd3df107f594c27bda070f727a6e63c203b033
cw2981 <cw2981>
2019-06-27 20:25:32 +0000

    added user id for photo upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_000530) do
+ActiveRecord::Schema.define(version: 2019_06_27_200923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_000530) do
     t.string "picture"
     t.string "caption"
     t.integer "place_id"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
