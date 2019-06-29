bdc409703c7b9cbf9456f06221fbba8def79b4f4
cw2981 <cw2981>
2019-06-29 20:49:52 +0000

    still trying to fix uploader errors.

--- Gemfile
+++ Gemfile
@@ -73,5 +73,5 @@ gem "font-awesome-rails"
 gem 'devise'
 gem "geocoder"
 gem "figaro"
-gem 'carrierwave', '~> 1.0'
+gem 'carrierwave', '>= 2.0.0.rc', '< 3.0'
 gem "fog-aws"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,8 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY"],
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    region: ENV["S3_REGION"]
+
   }
   config.fog_directory = ENV["AWS_BUCKET"]
 
