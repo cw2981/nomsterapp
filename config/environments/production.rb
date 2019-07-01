8235d930fee275ca222b79ba45cd69ec380fed47
cw2981 <cw2981>
2019-07-01 01:27:32 +0000

    attempting to debig sendgrid errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Rails.application.configure do
-   config.action_mailer.default_url_options = { host: 'nomsterapp-craig-white.herokuapp.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+   #config.action_mailer.default_url_options = { host: 'nomsterapp-craig-white.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
