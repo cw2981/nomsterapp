684b50d560529a145b906a9cfde93442ec896688
cw2981 <cw2981>
2019-07-01 01:43:00 +0000

    attempting to debug adding a comment email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-   #config.action_mailer.default_url_options = { host: 'nomsterapp-craig-white.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'nomsterapp-craig-white.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
